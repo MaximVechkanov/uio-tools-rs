0aef71c35c75f02c8a15730b19f01f1aa46005b3
Maxim Vechkanov <maxim.vechkanov@radiogigabit.com>
2023-05-28 19:28:45 +0300

    uio-list: Add support for name pattern

--- src/bin/uio-list.rs
+++ src/bin/uio-list.rs
@@ -16,26 +16,9 @@ struct CliParams {
 
 const UIO_CLASS_PATH: &str = "/sys/class/uio";
 
-fn print_properties(uio_name: String)
+fn print_maps(path: String)
 {
-    let path = UIO_CLASS_PATH.to_owned() + "/" + &uio_name;
-    println!("{}:", uio_name);
-
-    let name_filename = path.clone() + "/name";
-
-    let name_res = std::fs::read_to_string(&name_filename);
-
-    let name = match name_res {
-        Err(err) => {
-            println!("Failed to read uio name from '{}', err = {}", name_filename, err);
-            return;
-        },
-        Ok(name) => name,
-    };
-
-    print!("  name: {}", name);
-
-    let maps_res = std::fs::read_dir(path + "/maps");
+    let maps_res = std::fs::read_dir(path);
 
     let maps_dir = match maps_res {
         Err(..) => {
@@ -52,6 +35,40 @@ fn print_properties(uio_name: String)
         print!("    {}: {}", map_dir.unwrap().file_name().to_str().unwrap(), addr);
 
     }
+}
+
+fn print_properties(uio_name: String, name_pattern: Option<&String>)
+{
+    let path = UIO_CLASS_PATH.to_owned() + "/" + &uio_name;
+    
+
+    let name_filename = path.clone() + "/name";
+
+    let name_res = std::fs::read_to_string(&name_filename);
+
+    let name = match name_res {
+        Err(err) => {
+            println!("Failed to read uio name from '{}', err = {}", name_filename, err);
+            return;
+        },
+        Ok(name) => name,
+    };
+
+    if name_pattern.is_some() && !name.contains(name_pattern.unwrap()){
+        return;
+    }
+
+    println!("{}:", uio_name);
+    print!("  name: {}", name);
+
+    print_maps(path + "/maps");
+
+    for _ in 0..40 {
+        print!("-");
+    }
+    println!();
+
+
 }
 
 fn main() {
@@ -72,7 +89,7 @@ fn main() {
     let args = CliParams::parse();
 
     if args.index.is_some() {
-        print_properties("uio".to_owned() + &args.index.unwrap().to_string());
+        print_properties("uio".to_owned() + &args.index.unwrap().to_string(), None);
         return;
     }
 
@@ -81,10 +98,6 @@ fn main() {
 
     for uio_dir in dir {
         // uio0, uio1, ...
-        print_properties(uio_dir.unwrap().file_name().into_string().unwrap());
-        for _ in 0..40 {
-            print!("-");
-        }
-        println!();
+        print_properties(uio_dir.unwrap().file_name().into_string().unwrap(), args.name.as_ref());
     }
 }
